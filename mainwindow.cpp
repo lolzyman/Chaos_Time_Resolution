268a529aea04c74bfd2311c5ba739e9cdb2e8007
Mark <Mark Kent>
2020-02-03 19:55:14 -0500

    Generic Time dependent chaos equation discovered

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    displayGraph();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +14,71 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::displayGraph(){
+    generateRegularSeries();
+    generateLambdaSeriesOne();
+    generateLambdaSeriesTwo();
+    QLineSeries * weirdLambdaSeries = new QLineSeries();
+    generateGLambdaSeries(weirdLambdaSeries);
+
+    displayChart->legend()->hide();
+    displayChart->addSeries(regularSeries);
+    displayChart->addSeries(lambdaSeriesOne);
+    displayChart->addSeries(lambdaSeriesTwo);
+    displayChart->addSeries(weirdLambdaSeries);
+    displayChart->createDefaultAxes();
+    displayChart->setTitle("Time exploration of Chaos Theory");
+
+    chartViewer->setRenderHint(QPainter::Antialiasing);
+
+    this->setCentralWidget(chartViewer);
+    this->show();
+}
+
+void MainWindow::generateRegularSeries(){
+    double current_value = initial_value;
+    for(int i = 0; i < maxNIndex; i++){
+        regularSeries->append(i, current_value);
+        current_value = regularEquation(current_value);
+    }
+}
+
+void MainWindow::generateLambdaSeriesOne(){
+    double current_value = initial_value;
+    for(double i = 0; i < maxNIndex; i += lambdaOne){
+        lambdaSeriesOne->append(i, current_value);
+        current_value = lambdaEquationOne(current_value);
+    }
+}
+
+void MainWindow::generateLambdaSeriesTwo(){
+    double current_value = initial_value;
+    for(double i = 0; i < maxNIndex; i += lambdaTwo){
+        lambdaSeriesTwo->append(i, current_value);
+        current_value = lambdaEquationTwo(current_value);
+    }
+}
+
+void MainWindow::generateGLambdaSeries(QLineSeries *targetSeries){
+    double current_value = initial_value;
+    for(double i = 0; i < maxNIndex; i += gLambda){
+        targetSeries->append(i, current_value);
+        current_value = genericLambdaEquation(current_value);
+    }
+}
+
+double MainWindow::regularEquation(double value){
+    return r_constant * (value - value * value);
+}
+
+double MainWindow::lambdaEquationOne(double value){
+    return lambdaOne * (r_constant * (value - value * value) + value);
+}
+
+double MainWindow::lambdaEquationTwo(double value){
+    return lambdaTwo * r_constant * (value - value * value) + 3 * lambdaTwo * value;
+}
+
+double MainWindow::genericLambdaEquation(double value){
+    return gLambda * r_constant * (value - value * value) + (1 - gLambda) * value;
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QtCharts>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -14,8 +14,30 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    double lambdaOne = 0.5;
+    double lambdaTwo = 0.25;
+    double r_constant = 2;
+    double gLambda = 0.333333;
+    double initial_value = 0.015;
+    double maxNIndex = 12;
 
 private:
     Ui::MainWindow *ui;
+    QLineSeries *regularSeries = new QLineSeries();
+    QLineSeries *lambdaSeriesOne = new QLineSeries();
+    QLineSeries *lambdaSeriesTwo = new QLineSeries();
+    QChart *displayChart = new QChart();
+    QChartView *chartViewer = new QChartView(displayChart);
+
+    void displayGraph();
+    void generateRegularSeries();
+    void generateLambdaSeriesOne();
+    void generateLambdaSeriesTwo();
+    void generateGLambdaSeries(QLineSeries *targetSeries);
+    double lambdaEquationOne(double value);
+    double lambdaEquationTwo(double value);
+    double regularEquation(double value);
+    double genericLambdaEquation(double value);
+
 };
 #endif // MAINWINDOW_H
